3f825f9c7f7c303bf5ba7d8a102c663662533a7b
Anshul Kushwaha <anshulkushwaha311@gmail.com>
2025-01-30 10:06:52 +0530

    my second program

---
+++ B.java
@@ -0,0 +1,9 @@
+public class B {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+        System.out.println("d");
+        System.out.println("e");
+    }
+}
